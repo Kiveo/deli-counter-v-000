09c0f2203000bfb19e27b57ed7d8e85836f470d4
Maverick Tesini <mavericktesini@gmail.com>
2018-03-02 18:33:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,8 @@ def line(array)
   if array == []
   puts "The line is currently empty."
   else
-    y = array.each_with_index do |name, index|
-      puts "#{index+1}. #{name}"
-    end
+    y = array.each_with_index do { |name, index|
+      puts "#{index+1}. #{name}" }
     puts "The line is currently: #{y}"
   end
 end
